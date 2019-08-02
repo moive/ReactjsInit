d88550dd03a20fb7179975dd89a8294d79174ec2
Moises Velasquez <mvelasquez@pictoric.com>
2019-08-02 17:37:30 -0500

    App in arrow function

--- src/App.js
+++ src/App.js
@@ -2,12 +2,7 @@ import React from 'react';
 // import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div>This is my component: <Helloworld/></div>
-  );
-}
-
+const App = ()=> <div>This is my component: <Helloworld/></div>
 
 function Helloworld(){
   return (
