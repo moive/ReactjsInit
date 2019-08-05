0a6e739bb84593e40de76fe18b7fb7ee92f15c73
Moises Velasquez <mvelasquez@pictoric.com>
2019-08-05 08:57:16 -0500

    state

--- src/App.js
+++ src/App.js
@@ -2,6 +2,38 @@ import React from 'react';
 // import logo from './logo.svg';
 import './App.css';
 
+/*function Helloworld(props){
+  return (
+    <div>
+      <h3>{props.subtitle}</h3>
+      {props.text}
+    </div>
+  )
+}*/
+
+class Helloworld extends React.Component{
+  state = {
+    show:true
+  }
+
+  toggleShow = () => {
+    this.setState({show: !this.state.show});
+  }
+  render(){
+    if(this.state.show){
+      return (
+        <div>
+          <h3>{this.props.subtitle}</h3>
+            {this.props.text}
+            <button onClick={this.toggleShow}>Toggle Show</button>
+        </div>
+      );
+    }else{
+      return (<h1>There are not elements <button  onClick={this.toggleShow}>Toggle Show</button></h1>);
+    }
+  }
+}
+
 function App() {
   return <div>
     This is my component: 
@@ -9,15 +41,6 @@ function App() {
     <Helloworld text="Hola" subtitle="subtitle 2"/> 
     <Helloworld text="Hello" subtitle="This is a sub title"/>
   </div>
-}
-
-function Helloworld(props){
-  return (
-    <div>
-      <h3>{props.subtitle}</h3>
-      {props.text}
-    </div>
-  )
 }
 
 export default App;
