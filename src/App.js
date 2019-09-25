5217cc0b2736b5118214c14a96fad319c72866c7
Moises Velasquez <mvelasquez@pictoric.com>
2019-09-25 18:23:44 -0500

    init task app

--- src/App.js
+++ src/App.js
@@ -1,46 +1,19 @@
-import React from 'react';
-// import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import tasks from './sample/tasks.json';
 
-/*function Helloworld(props){
-  return (
-    <div>
-      <h3>{props.subtitle}</h3>
-      {props.text}
-    </div>
-  )
-}*/
-
-class Helloworld extends React.Component{
+class App extends Component{
   state = {
-    show:true
+    tasks
   }
 
-  toggleShow = () => {
-    this.setState({show: !this.state.show});
-  }
   render(){
-    if(this.state.show){
-      return (
-        <div>
-          <h3>{this.props.subtitle}</h3>
-            {this.props.text}
-            <button onClick={this.toggleShow}>Toggle Show</button>
-        </div>
-      );
-    }else{
-      return (<h1>There are not elements <button  onClick={this.toggleShow}>Toggle Show</button></h1>);
-    }
+    return <div>
+      {this.state.tasks.map((e)=><p key={e.id}>
+        {e.title} - {e.description} - {e.done} - {e.id}
+      </p>)}
+    </div>
   }
-}
-
-function App() {
-  return <div>
-    This is my component: 
-    <Helloworld text="Hi..!" subtitle="Lorem ipsum"/> 
-    <Helloworld text="Hola" subtitle="subtitle 2"/> 
-    <Helloworld text="Hello" subtitle="This is a sub title"/>
-  </div>
 }
 
 export default App;
