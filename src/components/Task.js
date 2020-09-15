e9f7da85bdb533598178c828c54ffb9e37ecd739
Moises Velasquez <mvelasquez@pictoric.com>
2020-09-14 22:36:12 -0500

    working with style css

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,14 +1,29 @@
 import React, { Component } from 'react';
 
 class Task extends Component {
+    isDone(){
+        return this.props.task.done;
+    }
     render() {
         const { task } = this.props;
-        return <div>
+        return <div style={this.styleCompleted()}>
             {task.title} - {task.description} - {task.done} - {task.id}
-            <input type="checkbox"/>
-            <button className="btn btn-danger">x</button>
+            <input type="checkbox" disabled={this.isDone()} />
+            <button style={btnDelete} className="btn btn-danger" disabled={this.isDone()}>x</button>
         </div>
     };
+
+    styleCompleted(){
+        return {
+            color: this.isDone() ? 'gray' : 'black',
+            textDecoration: this.isDone() ? 'line-through' : 'none'
+        }
+    }
+}
+
+const btnDelete = {
+    fontSize: '25px',
+    fontWeight: 'bold'
 }
 
 export default Task;
