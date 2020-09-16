af9cbe55edc59090daa146978c6427e5fbb8666a
Moises Velasquez <mvelasquez@pictoric.com>
2020-09-16 00:03:32 -0500

    React Router

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import './App.css';
 import tasks from './sample/tasks.json';
 
@@ -42,13 +43,37 @@ class App extends Component{
   }
   render(){
     return <div className="container">
-      <TaskForm addTask={this.addTask} />
-      <Tasks 
-        tasks={this.state.tasks} 
-        deleteTask={this.deleteTask} 
-        checkDone={this.checkDone} 
-      />
-      <Posts/>
+      <Router>
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+          <Link className="navbar-brand" to="/">Navbar</Link>
+          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav mr-auto">
+              <li className="nav-item active">
+                <Link className="nav-link" to="/">Home</Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/posts">Posts</Link>
+              </li>
+            </ul>
+          </div>
+        </nav>
+        <Route path="/" exact render={()=>{
+          return <div>
+            <TaskForm addTask={this.addTask} />
+            <Tasks 
+              tasks={this.state.tasks} 
+              deleteTask={this.deleteTask} 
+              checkDone={this.checkDone} 
+            />
+          </div>
+        }}>
+        </Route>
+        <Route path="/posts" component={Posts} />
+      </Router>
     </div>
   }
 }
--- src/components/Posts.js
+++ src/components/Posts.js
@@ -8,7 +8,7 @@ export default class Posts extends Component {
     async componentDidMount(){
         const res = await fetch('http://jsonplaceholder.typicode.com/posts')
         const data = await res.json();
-        console.log(data);
+        // console.log(data);
         this.setState({
             posts: data
         });
@@ -20,7 +20,7 @@ export default class Posts extends Component {
                 {
                     this.state.posts.map(post => {
                         return <div key={post.id}>
-                            <h3 className={post.id%2==0 ? 'text-success': 'text-primary'}>{post.title}</h3>
+                            <h3 className={post.id%2 === 0 ? 'text-success': 'text-primary'}>{post.title}</h3>
                             <p>{post.body}</p>
                         </div>
                     })
